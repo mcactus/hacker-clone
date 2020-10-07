a5dd6714bac68345b110b3b10c9ab23f94dee981
mcactus <pavelboloshovtpr@gmail.com>
2020-10-07 14:56:58 +0300

    included bootstrap 5 ui kit

--- src/components/ArticlesPage.js
+++ src/components/ArticlesPage.js
@@ -7,28 +7,28 @@ import {Link} from 'react-router-dom';
 const ArticlesPage = () => {
   const [articles, setArticles] = useState([]);
   useEffect(() => {
-    async function getArticles() {
+    const getArticles = async () => {
         const { hits } = await RequestProvider.getHomePage();
         setArticles(hits);
     }
 
     getArticles();
-  })
+  }, []);
 
   return (
     <main className='container'>
       <div className='row row-cols-1 row-cols-lg-3 g-4'>
-        {articles.map(({ id, url, title, author, points, num_comments }) => {
+        {articles.map(({ objectID, url, title, author, points, num_comments }) => {
           const articleSource = url ? (
             <a
               href={url}
               target='_blank'
               className='d-inline mr-2 link-light text-uppercase'
               title='Read on source'>
-              <i className='fas fa-location-arrow'/> src
+              Read on source
             </a>) : '';
           return (
-            <div className='col'>
+            <div className='col' key={objectID}>
               <div className='card bg-dark text-light h-100'>
                 <div className='card-header'> {author} </div>
                 <div className='card-body'>
@@ -36,13 +36,13 @@ const ArticlesPage = () => {
                 </div>
                 <div className='card-footer'>
                   <div className='d-inline mr-2' title='Upvotes'>
-                    <i className='far fa-thumbs-up'/> {points}
+                    Upvotes ({points})
                   </div>
                   <Link
-                    to={`/article/${id}`}
+                    to={`/article/${objectID}`}
                     className='d-inline mr-2 link-light history-link'
                     title='Comments'>
-                    <i className='far fa-comment'/> {num_comments}
+                    Comments ({num_comments})
                   </Link>
                   {articleSource}
                 </div>
--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {BrowserRouter as Router} from 'react-router-dom';
+import 'mdb-ui-kit/css/mdb.min.css';
 
 import App from './components/App';
 
